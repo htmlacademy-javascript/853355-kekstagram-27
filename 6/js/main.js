ecbe96597770c8837075bb542bba16bd26295f8b
Keks <keks@htmlacademy.ru>
2022-11-03 17:28:00 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,3 @@
+import './posts.js';
+import './popup.js';
+
---
+++ 6/js/posts.js
@@ -0,0 +1,37 @@
+import { getPosts } from './data.js';
+import {showPopup} from './popup.js';
+
+const postsContainer = document.querySelector('.pictures');
+const userPostTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const usersPosts = getPosts();
+
+const createPost = (data) => {
+  const {comments, description, likes, url} = data;
+  const userPostElement = userPostTemplate.cloneNode(true);
+  userPostElement.querySelector('.picture__img').src = url;
+  userPostElement.querySelector('.picture__img').alt = description;
+  userPostElement.querySelector('.picture__comments').textContent = comments.length;
+  userPostElement.querySelector('.picture__likes').textContent = likes;
+
+  userPostElement.addEventListener('click', () => {
+    showPopup(data);
+  });
+
+  return userPostElement;
+};
+
+const renderPosts = (posts) => {
+  const fragment = document.createDocumentFragment();
+  posts.forEach((post) => {
+    const postElement = createPost(post);
+    fragment.append(postElement);
+  });
+
+  postsContainer.append(fragment);
+};
+
+renderPosts(usersPosts);
+
