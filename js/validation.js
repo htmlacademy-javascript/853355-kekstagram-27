c7e11780120414d098df85e07ea7a25a345a35aa
Amir Farkhadov <farkhadov95@mail.ru>
2022-11-14 12:34:32 +0000

    HW 8.21 fix 1.3

--- js/validation.js
+++ js/validation.js
@@ -15,7 +15,7 @@ const containsDuplicates = (array) => {
 const checkLength = (tagsArray) => {
   if (tagsArray.length <= 5) {
     return true;
-  };
+  }
   return false;
 };
 
@@ -24,7 +24,7 @@ const allTagsValid = (tag) => regex.test(tag);
 const checkForm = () => {
   const tags = hashtagInput.value.trimEnd().split(' ');
 
-  const allChecks = [checkLength(tags),  !containsDuplicates(tags), tags.every(allTagsValid)];
+  const allChecks = [checkLength(tags), !containsDuplicates(tags), tags.every(allTagsValid)];
   const isTrue = (fun) => fun === true;
   return allChecks.every(isTrue);
 };
