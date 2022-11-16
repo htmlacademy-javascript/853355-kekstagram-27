32f91dbee5a4aa2c95c84c5a582cae9f1f482d76
Amir Farkhadov <farkhadov95@mail.ru>
2022-11-16 11:37:42 +0000

    HW 8.21 fix 2.3

--- js/validation.js
+++ js/validation.js
@@ -2,11 +2,9 @@ const hashtagInput = document.querySelector('.text__hashtags');
 
 const regex = /^#[a-zа-яё0-9\s]{1,19}$/;
 
-const containsDuplicates = (array) => {
-  return array.some((element) => array.indexOf(element) !== array.lastIndexOf(element));
-};
+const containsDuplicates = (array) => array.some((element) => array.indexOf(element) !== array.lastIndexOf(element));
 
-const checkLength = (tagsArray) =>  tagsArray.length <= 5;
+const checkLength = (tagsArray) => tagsArray.length <= 5;
 
 const isTagValid = (tag) => regex.test(tag);
 
