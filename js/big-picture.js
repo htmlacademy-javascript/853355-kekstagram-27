afa749d64dc75927703ff891f372284c2e673dd7
Amir Farkhadov <farkhadov95@mail.ru>
2022-11-17 18:26:33 +0000

    HW 8.22

--- js/big-picture.js
+++ js/big-picture.js
@@ -7,11 +7,56 @@ const commentsList = document.querySelector('.social__comments');
 const commentsCount = document.querySelector('.social__comment-count');
 const commentsLoader = document.querySelector('.social__comments-loader');
 
+const visibleComments = (comments) => {
+  const allComments = document.querySelectorAll('.social__comment');
+  let visibleCommentsCount = 5;
+
+  const checkVisibleCommentsCount = () => {
+    if (comments.childNodes.length < visibleCommentsCount) {
+       return visibleCommentsCount = comments.childNodes.length;
+    }
+  };
+
+  const checkLoader = () => {
+    if (comments.childNodes.length < 5 || comments.childNodes.length === visibleCommentsCount) {
+      visibleCommentsCount = comments.childNodes.length;
+      commentsLoader.classList.add('hidden');
+    } else {
+      commentsLoader.classList.remove('hidden');
+    }
+  };
+
+  const showSomeComments = (num) => {
+    checkVisibleCommentsCount();
+    checkLoader();
+    for (var i = 0; i < num; i++) {
+      if ( allComments[i].classList.contains('hidden')) {
+        allComments[i].classList.remove('hidden');
+      }
+    };
+  };
+
+  commentsLoader.addEventListener('click', () => {
+    visibleCommentsCount += 5;
+    if (comments.childNodes.length < visibleCommentsCount) {
+      visibleCommentsCount = comments.childNodes.length;
+    }
+    commentsCount.innerHTML = `${visibleCommentsCount} из <span class="comments-count">${comments.childNodes.length}</span> комментариев`;
+    checkVisibleCommentsCount();
+    showSomeComments(visibleCommentsCount);
+  });
+
+  checkVisibleCommentsCount();
+  commentsCount.innerHTML = `${visibleCommentsCount} из <span class="comments-count">${comments.childNodes.length}</span> комментариев`;
+  showSomeComments(visibleCommentsCount);
+}
+
 const createComment = ({avatar, name, message}) => {
   const comment = document.createElement('li');
   comment.innerHTML =
    `<img class="social__picture" src="${avatar}" alt="${name}" width="35" height="35"><p class="social__text">${message}</p>`;
   comment.classList.add('social__comment');
+  comment.classList.add('hidden');
   return comment;
 };
 
@@ -22,8 +67,8 @@ const renderComments = (comments) => {
     const commentElement = createComment(comment);
     fragment.append(commentElement);
   });
-
   commentsList.append(fragment);
+  visibleComments(commentsList);
 };
 
 const renderPostDetails = ({url, likes, description}) => {
@@ -50,8 +95,6 @@ const onPopupEscKeydown = (evt) => {
 const showPopup = (postData) => {
   pictureDialog.classList.remove('hidden');
   body.classList.add('modal-open');
-  commentsCount.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
 
   document.addEventListener('keydown', onPopupEscKeydown);
 
